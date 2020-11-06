f24af25485758eab2871004718344f2a981a4493
GardaAlex <aleex.dorin@gmail.com>
2020-11-06 11:59:40 +0200

    This is my first commit for Racing Game

---
+++ src/main/java/org/RacingGameExperiments/App.java
@@ -0,0 +1,13 @@
+package org.RacingGameExperiments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
